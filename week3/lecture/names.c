ffb0b4031be7fdce82cb99d7c94d74ea8c02d4ac
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2022-12-01 23:59:27 -0500

    completed a few problems from the week 3 lecture

---
+++ week3/lecture/names.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char *names[] = {
+        "Bill",
+        "Charlie",
+        "Fred",
+        "George",
+        "Ginny",
+        "Percy",
+        "Ron",
+    };
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (strcmp(names[i], "Ron") == 0)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
